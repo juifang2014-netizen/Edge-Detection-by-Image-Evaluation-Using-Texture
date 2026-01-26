1cce3896f2c2f954dc5d2bfea0d8817a39b85228
juifang2014-netizen <juifang2014@gmail.com>
2026-01-26 17:22:14 +0800

    Delete function directory

--- function/computeAUCPR.m
+++
@@ -1,9 +0,0 @@
-function AUCPR = computeAUCPR(recall, precision)
-    recall = recall(:); precision = precision(:);
-    valid = isfinite(recall) & isfinite(precision);
-    recall = recall(valid); precision = precision(valid);
-    if numel(recall)<2, AUCPR=NaN; return; end
-    [recall, idx] = sort(recall); precision = precision(idx);
-    [recall, ia] = unique(recall,'stable'); precision = precision(ia);
-    AUCPR = trapz(recall,precision);
-end
